73540b697c602eab53716ab00cb8088b938b4763
Pritam <pritammahata12@gmail.com>
2024-08-29 08:02:23 +0530

    comment out model js

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,13 +28,13 @@ const modalCloseBtn = document.querySelector("[data-modal-close]");
 const modalCloseOverlay = document.querySelector("[data-modal-overlay]");
 
 // modal function
-const modalCloseFunc = function () {
-  modal.classList.add("closed");
-};
-
-// modal eventListener
-modalCloseOverlay.addEventListener("click", modalCloseFunc);
-modalCloseBtn.addEventListener("click", modalCloseFunc);
+// const modalCloseFunc = function () {
+//   modal.classList.add("closed");
+// };
+//
+// // modal eventListener
+// modalCloseOverlay.addEventListener("click", modalCloseFunc);
+// modalCloseBtn.addEventListener("click", modalCloseFunc);
 
 // notification toast variables
 const notificationToast = document.querySelector("[data-toast]");
