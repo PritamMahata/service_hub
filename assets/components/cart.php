3444fcf5f8e1a203b222d262490096988804c199
Pritam <pritammahata12@gmail.com>
2024-08-30 22:38:31 +0530

    addning cart page

---
+++ assets/components/cart.php
@@ -0,0 +1,20 @@
+<div class="cart-container">
+    <h1>Shopping Cart</h1>
+    <ul class="cart-items">
+        <li class="cart-item">
+            <span class="item-name">Item 1</span>
+            <span class="item-quantity">1</span>
+            <span class="item-price">$10.00</span>
+        </li>
+        <li class="cart-item">
+            <span class="item-name">Item 2</span>
+            <span class="item-quantity">2</span>
+            <span class="item-price">$20.00</span>
+        </li>
+        <!-- Add more items as needed -->
+    </ul>
+    <div class="cart-total">
+        <span>Total:</span>
+        <span class="total-price">$30.00</span>
+    </div>
+</div>
