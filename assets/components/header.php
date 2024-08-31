4f392038202fefbffd5bdda46945226a2195ba15
Pritam <pritammahata12@gmail.com>
2024-08-31 17:15:17 +0530

    link the logo with index.php, try to create dynamic login signup button

--- assets/components/header.php
+++ assets/components/header.php
@@ -1,7 +1,11 @@
+<?php
+session_start();
+$isLoggedIn = isset($_SESSION['uid']);
+?>
 <header>
   <div class="header-main">
     <div class="container">
-      <a href="#" class="header-logo">
+      <a href="./index.php" class="header-logo">
         <img src="./assets/images/logo/logo.png" width="120" height="36">
       </a>
       <div class="header-search-container">
@@ -11,17 +15,31 @@
         </button>
       </div>
       <div class="header-user-actions">
-        <button class="action-btn">
+        <!-- wishlist -->
+        <!-- <button class="action-btn">
           <ion-icon name="heart"></ion-icon>
           <span class="count">5</span>
-        </button>
+        </button> -->
         <button class="action-btn">
           <ion-icon name="cart"></ion-icon>
           <span class="count">2</span>
         </button>
-        <button class="action-btn">
+
+        <?php if ($isLoggedIn) { ?>
+          <button class="action-btn" onclick="window.location.href = './logout.php'">
+            <ion-icon name="log-out-outline"></ion-icon>
+          </button>
+        <?php } else { ?>
+          <button class="action-btn" onclick="window.location.href = './login.php'">
+            <ion-icon name="log-in-outline"></ion-icon>
+          </button>
+        <?php } ?>
+
+        <!-- <button class="action-btn">
           <ion-icon name="person-circle-outline"></ion-icon>
-        </button>
+        </button> -->
+
+
       </div>
     </div>
   </div>
