1df926aae615fc7790ad21531aa8963951b9bba0
Pritam <pritammahata12@gmail.com>
2024-09-02 23:14:52 +0530

    add hotlines in banner

--- assets/components/banner.php
+++ assets/components/banner.php
@@ -3,32 +3,33 @@
     <div class="container">
       <div class="slider-container has-scrollbar">
         <div class="slider-item">
-          <img src="./assets/images/banner-1.jpg"  class="banner-img">
+          <img src="./assets/images/banner-1.jpg" class="banner-img">
           <div class="banner-content">
             <p class="banner-subtitle">Lorem ipsum </p>
-            <h2 class="banner-title">Lorem ipsum dolor sit amet consecrate</h2>
+            <h2 class="banner-title" id="hotline"></h2>
             <p class="banner-text"> starting at &dollar; <b>20</b>.00 </p>
-            <a href="#" class="banner-btn">Shop now</a>
+            <a href="#" class="banner-btn">Book Now</a>
           </div>
         </div>
         <div class="slider-item">
           <img src="./assets/images/banner-2.jpg" class="banner-img">
           <div class="banner-content">
             <p class="banner-subtitle">Lorem ipsum </p>
-            <h2 class="banner-title">Lorem ipsum dolor sit amet consecrate</h2>
+            <h2 class="banner-title" id="hotline"></h2>
             <p class="banner-text"> starting at &dollar; <b>15</b>.00 </p>
-            <a href="#" class="banner-btn">Shop now</a>
+            <a href="#" class="banner-btn">Book Now</a>
           </div>
         </div>
         <div class="slider-item">
-          <img src="./assets/images/banner-3.jpg"  class="banner-img">
+          <img src="./assets/images/banner-3.jpg" class="banner-img">
           <div class="banner-content">
             <p class="banner-subtitle">Sale Offer</p>
-            <h2 class="banner-title">Lorem ipsum dolor sit amet consecrate</h2>
+            <h2 class="banner-title" id="hotline"></h2>
             <p class="banner-text"> starting at &dollar; <b>29</b>.99 </p>
-            <a href="#" class="banner-btn">Shop now</a>
+            <a href="#" class="banner-btn">Book Now</a>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <!-- <script src="./assets/js/hotline.js"></script> -->
