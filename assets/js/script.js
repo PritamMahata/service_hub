6c168f5c67e27dfc7d05f3fc01de185470dd4cc0
Pritam <pritammahata12@gmail.com>
2024-09-02 23:16:23 +0530

    remove hot lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,5 @@
 "use strict";
 
-// hot lines
-// let hotlines = [
-//   "Your One-Stop Solution for Service Needs!",
-//   "Book Smarter, Live Better!",
-//   "Convenience at Your Fingertips!",
-//   "Simplify Your Schedule, Amplify Your Life!",
-//   "Effortless Booking, Exceptional Service!",
-//   "Unlock a World of Services, Anytime, Anywhere!",
-//   "Book with Ease, Enjoy with Peace!",
-//   "Where Service Meets Simplicity!",
-//   "Your Trusted Partner in Booking Services!",
-//   "The Fast Track to Hassle-Free Service!",
-// ];
-// const hotline = document.getElementById("hotline");
-// let index = 0;
-// function updateHotline() {
-//   hotline.innerHTML = `<h2>${hotlines[index]}</h2>`;
-//   index = (index + 1) % hotlines.length;
-// }
-// updateHotline();
-// setInterval(updateHotline, 10000);
-
-// modal variables
-const modal = document.querySelector("[data-modal]");
-const modalCloseBtn = document.querySelector("[data-modal-close]");
-const modalCloseOverlay = document.querySelector("[data-modal-overlay]");
-
-// modal function
-// const modalCloseFunc = function () {
-//   modal.classList.add("closed");
-// };
-//
-// // modal eventListener
-// modalCloseOverlay.addEventListener("click", modalCloseFunc);
-// modalCloseBtn.addEventListener("click", modalCloseFunc);
-
 
 
 // mobile menu variables
