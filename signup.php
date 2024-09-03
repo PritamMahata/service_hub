b809ff405ac85abc765e49eee2b63e03d20c19cf
Pritam <pritammahata12@gmail.com>
2024-09-03 20:49:41 +0530

    register data through signup form

--- signup.php
+++ signup.php
@@ -30,13 +30,53 @@
             </div>
             <div style="min-width:350px" class="form_style">
 
-            <h1 class="form_heading">Create Account</h1>
-            <div class="row_field">
-                <div class="sidebyside ">
-                    <p class="nav-title"> Already have any account !</p> <a href="./login.php">Login</a>
+                <h1 class="form_heading">Create Account</h1>
+                <div class="row_field">
+                    <div class="sidebyside ">
+                        <p class="nav-title"> Already have any account !</p> <a href="./login.php">Login</a>
+                    </div>
                 </div>
-            </div>
-                <form action="#">
+                <?php
+                if (isset($_POST['ok'])) {
+                    $fname = $_POST['fname'];
+                    $mname = $_POST['mname'];
+                    $lname = $_POST['lname'];
+                    $gender = NULL;
+                    $age = NULL;
+                    $email = $_POST['email'];
+                    $con_num = $_POST['con_num'];
+                    $alt_num = $_POST['alt_num'];
+                    $address = $_POST['address'];
+                    $password = $_POST['password'];
+                    $acc_type = $_POST['acc_type'];
+                    $pan_card = $_POST['pan_card'];
+                    $acc_num = $_POST['acc_num'];
+                    $ifsc = $_POST['ifsc'];
+                    $experience = NULL;
+                    $photo = NULL;
+                    $certificate = NULL;
+                    $aadhaar = NULL;
+                    $sql = "INSERT INTO PROVIDER (fname, mname, lname, gender, age, email, con_num, alt_num, address, password, acc_type, pan_card, acc_num, ifsc, experience, photo, certificate, aadhaar) VALUES ('$fname', '$mname', '$lname', '$gender', '$age', '$email', $con_num, $alt_num, '$address', '$password', '$acc_type', '$pan_card', $acc_num, '$ifsc', '$experience', '$photo', '$certificate', '$aadhaar')";
+                    $res = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+                    if ($res == 1) {
+                        echo "<script>alert('Registration is Successfull')</script>";
+                ?>
+                        <br>
+                        <div class="alert alert-success">
+                            Registration is Successfull
+                        </div>
+                    <?php
+                    } else {
+                    ?>
+                        <br>
+                        <div class="alert alert-danger">
+                            Registration is Unsuccessfull
+                        </div>
+                <?php
+                    }
+                }
+                ?>
+                <form method="POST">
                     <div class="col_field">
                         <div class="row_field">
                             <label class="newsletter-title">First Name</label>
@@ -83,70 +123,17 @@
                         <textarea id="w3review" name="address" rows="4" cols="50"></textarea>
                     </div>
                     <br>
-                    <label class="newsletter-title">Account Type</label>
-                    <select class="email-field" name="acc_type" id="acc_type" onchange="test()">
-                        <option value="1">Consumer</option>
-                        <option value="2">Provider</option>
-                    </select>
-                    <br>
-                    <div id="check" style="display: none;">
-                        <div class="row_field">
-                            <label class="newsletter-title">PAN Card</label>
-                            <input type="text" name="pan_card" class="email-field" placeholder="PAN Card Number" required>
+                    <div class="col_field">
+                        <div class="container flex_div">
+                            <button name="ok" class="btn-newsletter disable" onclick="window.history.back();"> Back</button>
                         </div>
-                        <div class="row_field">
-                            <label class="newsletter-title">Bank Account Details</label>
-                            <br>
-                            <label class="newsletter-title">Account Number</label>
-                            <input type="text" name="acc_num" class="email-field" placeholder="Account Number" required>
+                        <div class="container flex_div">
+                            <button type="submit" name="ok" class="btn-newsletter">Sign Up</button>
                         </div>
-                        <div class="row_field">
-                            <label class="newsletter-title">IFSC Code</label>
-                            <input type="text" name="ifsc" class="email-field" placeholder="IFSC Code" required>
-                        </div>
-                    </div><br>
-                    <div class="container flex_div">
-                        <button type="submit" name="ok" class="btn-newsletter">Sign Up</button>
                     </div>
                 </form>
 
-                <?php
-                if (isset($_POST['ok'])) {
-                    $fname = $_POST['fname'];
-                    $mname = $_POST['mname'];
-                    $lname = $_POST['lname'];
-                    $gender = NULL;
-                    $age = NULL;
-                    $email = $_POST['email'];
-                    $con_num = $_POST['con_num'];
-                    $alt_num = $_POST['alt_num'];
-                    $address = $_POST['address'];
-                    $password = $_POST['password'];
-                    $acc_type = $_POST['acc_type'];
-                    $pan_card = $_POST['pan_card'];
-                    $acc_num = $_POST['acc_num'];
-                    $ifsc = $_POST['ifsc'];
-                    $experience = NULL;
-                    $photo = NULL;
-                    $certificate = NULL;
-                    $aadhaar = NULL;
-                    $sql = "INSERT INTO PROVIDER (fname, mname, lname, gender, age, email, con_num, alt_num, address, password, acc_type, pan_card, acc_num, ifsc, experience, photo, certificate, aadhaar) VALUES ('$fname', '$mname', '$lname', '$gender', '$age', '$email', $con_num, $alt_num, '$address', '$password', '$acc_type', '$pan_card', $acc_num, '$ifsc', '$experience', '$photo', '$certificate', '$aadhaar')";
-                    $res = mysqli_query($con, $sql) or die(mysqli_error($con));
-                    if ($res == 1) {
-                        ?>
-                        <br><div class="alert alert-success">
-                            Registration is Successfull
-                        </div>
-                        <?php
-                    } else {
-                    ?>
-                        <br><div class="alert alert-success">
-                            Registration is Unsuccessfull
-                        </div>
-                    <?php
-                    }
-                }
-                ?>
+
             </div>
         </div>
     </div>
