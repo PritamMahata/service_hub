d9d6502301d9dd7d3c33b398bee860dcdefbe2c7
Pritam <pritammahata12@gmail.com>
2024-09-04 17:27:32 +0530

    adding menu bar option

--- assets/components/sub_components/menubar/menubar.php
+++ assets/components/sub_components/menubar/menubar.php
@@ -8,13 +8,27 @@
     <ion-icon name="cart"></ion-icon>
     <span class="count">0</span>
   </button>
-  <button class="action-btn">
+  <button class="action-btn" onclick="window.location.href='./index.php'">
     <ion-icon name="home-outline"></ion-icon>
   </button>
-  <button class="action-btn">
+
+  <?php
+      if ($isLoggedIn) { ?>
+        <button class="action-btn" onclick="window.location.href = './task.php'">
+          <ion-icon name="calendar"></ion-icon>
+          <span class="count">2</span>
+        </button>
+      <?php } else { ?>
+        <button class="action-btn" onclick="window.location.href = './login.php'">
+          <ion-icon name="log-in"></ion-icon>
+        </button>
+      <?php }
+      ?>
+  <!-- <button class="action-btn">
     <ion-icon name="heart"></ion-icon>
     <span class="count">0</span>
-  </button>
+  </button> -->
+
   <button class="action-btn" data-mobile-menu-open-btn>
     <ion-icon name="grid-outline"></ion-icon>
   </button>
