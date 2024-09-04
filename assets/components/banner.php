9f20d255c165f403e0883ec87dce951b661b4f8d
Pritam <pritammahata12@gmail.com>
2024-09-05 01:02:40 +0530

    activate hot lines

--- assets/components/banner.php
+++ assets/components/banner.php
@@ -5,31 +5,31 @@
         <div class="slider-item">
           <img src="./assets/images/banner-1.jpg" class="banner-img">
           <div class="banner-content">
-            <p class="banner-subtitle">Lorem ipsum </p>
+            <!-- <p class="banner-subtitle">Lorem ipsum </p> -->
             <h2 class="banner-title" id="hotline"></h2>
-            <p class="banner-text"> starting at &dollar; <b>20</b>.00 </p>
-            <a href="#" class="banner-btn">Book Now</a>
+            <!-- <p class="banner-text"> starting at &dollar; <b>20</b>.00 </p> -->
+            <!-- <a href="#" class="banner-btn">Book Now</a> -->
           </div>
         </div>
         <div class="slider-item">
           <img src="./assets/images/banner-2.jpg" class="banner-img">
           <div class="banner-content">
-            <p class="banner-subtitle">Lorem ipsum </p>
+            <!-- <p class="banner-subtitle">Lorem ipsum </p> -->
             <h2 class="banner-title" id="hotline"></h2>
-            <p class="banner-text"> starting at &dollar; <b>15</b>.00 </p>
-            <a href="#" class="banner-btn">Book Now</a>
+            <!-- <p class="banner-text"> starting at &dollar; <b>15</b>.00 </p> -->
+            <!-- <a href="#" class="banner-btn">Book Now</a> -->
           </div>
         </div>
         <div class="slider-item">
           <img src="./assets/images/banner-3.jpg" class="banner-img">
           <div class="banner-content">
-            <p class="banner-subtitle">Sale Offer</p>
+            <!-- <p class="banner-subtitle">Sale Offer</p> -->
             <h2 class="banner-title" id="hotline"></h2>
-            <p class="banner-text"> starting at &dollar; <b>29</b>.99 </p>
-            <a href="#" class="banner-btn">Book Now</a>
+            <!-- <p class="banner-text"> starting at &dollar; <b>29</b>.99 </p> -->
+            <!-- <a href="#" class="banner-btn">Book Now</a> -->
           </div>
         </div>
       </div>
     </div>
   </div>
-  <!-- <script src="./assets/js/hotline.js"></script> -->
+  <script src="./assets/js/hotline.js"></script>
