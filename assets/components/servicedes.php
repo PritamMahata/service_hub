5c81c35bc21579673876ac9d8a5f22ef4232adf8
Pritam <pritammahata12@gmail.com>
2024-09-06 20:43:06 +0530

    fix service description

--- assets/components/servicedes.php
+++ assets/components/servicedes.php
@@ -15,75 +15,80 @@ if ($result->num_rows > 0) {
         <div class="showcase-container" style="padding:10px;">
             <div class="showcase">
                 <div class="showcase-banner">
-                    <img src="./assets/images/services/img_5.jpg" class="showcase-img">
+                    <img src="<?php echo $row["simage"]?>" class="showcase-img">
                 </div>
                 <div class="showcase-content">
                     <a href="#">
-                        <h3 class="showcase-title" style="font-size: 1.6rem;">Lorem ipsum dolor sit amet consecrate</h3>
+                        <h3 class="showcase-title" style="font-size: 1.6rem;"><?php echo $row["sname"] ?></h3>
                     </a>
-                    <p class="showcase-desc" style="font-size: 1rem;"> Lorem ipsum dolor sit amet consecrate Lorem ipsum dolor dolor sit amet
-                        consectetur Lorem ipsum dolor
-                    </p>
+                    <p class="showcase-desc" style="font-size: 1rem;"><?php echo $row["sdes"] ?></p>
                     <div class="showcase-rating">
-                        <ion-icon name="star"></ion-icon>
-                        <ion-icon name="star"></ion-icon>
-                        <ion-icon name="star"></ion-icon>
-                        <ion-icon name="star-outline"></ion-icon>
-                        <ion-icon name="star-outline"></ion-icon>
+                        <?php
+                        $i = 0;
+                        for ($i; $i < htmlspecialchars($row['srating']); $i++) {
+                            echo "<ion-icon name='star'></ion-icon>";
+                        }
+                        for ($i; $i < 5; $i++) {
+                            echo "<ion-icon name='star-outline'></ion-icon>";
+                        }
+                        ?>
                     </div>
                     <div class="price-box">
-                        <p class="price">$150.00</p>
-                        <del>$200.00</del>
+                        <?php
+                        // echo "<p class='price'>$" . (($row['sprice']) - ((int)($row['sdiscount']) / 100) * (int)($row['sprice'])) . "</p>";
+                        // echo "<del>$" . htmlspecialchars($row['sprice']) . "</del>";
+                        ?>
+                        <p class="price"><?php echo "$" . (($row['sprice']) - ((int)($row['sdiscount']) / 100) * (int)($row['sprice'])) ?></p>
+                        <del><?php echo "$" . $row['sprice'] ?></del>
                     </div>
                     <div class="countdown-box">
                         <p class="countdown-desc">Date Available</p>
                         <div class="countdown">
+                            <?php
+                            $count = date("j");
+                            $date = new DateTime();
+                            $stopDay = 10;
+                            $startDayOfYear = (int)$date->format('z') + 1;
+                            $targetDayOfYear = $startDayOfYear + $stopDay - 1;
+                            while ((int)$date->format('z') + 1 != $targetDayOfYear) {
+                                echo '
+                                <div class="countdown-content">
+                                    <p class="display-number">' . $date->format('jS') . '</p>
+                                    <p class="display-text">' . $date->format('M') . '</p>
+                                </div>
+                                ';
+                                $date->modify('+1 day');
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="countdown-box">
+                        <p class="countdown-desc">Time Available</p>
+                        <div class="countdown">
                             <div class="countdown-content">
-                                <p class="display-number">03</p>
-                                <p class="display-text">Aug</p>
+                                <p class="display-number">9:00 AM - 12:00 PM</p>
                             </div>
                             <div class="countdown-content">
-                                <p class="display-number">03</p>
-                                <p class="display-text">Aug</p>
+                                <p class="display-number">12:00 PM - 3:00 PM</p>
                             </div>
                             <div class="countdown-content">
-                                <p class="display-number">03</p>
-                                <p class="display-text">Aug</p>
-                            </div>
-                            <div class="countdown-content">
-                                <p class="display-number">03</p>
-                                <p class="display-text">Aug</p>
+                                <p class="display-number">3:00 PM - 5:00 PM</p>
                             </div>
                         </div>
                     </div>
 
                     <div class="container" style="margin:30px 0">
                         <ul>
-                            <li class="showcase-list">
-                                <div class="sidebyside">
-                                    <ion-icon name="chevron-forward"></ion-icon>
-                                    <p> Lorem ipsum dolor sit amet consecrate</p>
-                                </div>
-                            </li>
-                            <li class="showcase-list">
-                                <div class="sidebyside">
-                                    <ion-icon name="chevron-forward"></ion-icon>
-                                    <p> Lorem ipsum dolor sit amet consecrate</p>
-                                </div>
-                            </li>
-                            <li class="showcase-list">
-                                <div class="sidebyside">
-                                    <ion-icon name="chevron-forward"></ion-icon>
-                                    <p> Lorem ipsum dolor sit amet consecrate</p>
-                                </div>
-                            </li>
-                            <li class="showcase-list">
-                                <div class="sidebyside">
-                                    <ion-icon name="chevron-forward"></ion-icon>
-                                    <p> Lorem ipsum dolor sit amet consecrate</p>
-                                </div>
-                            </li>
-
+                            <?php
+                            $features = explode(',', $row['sfeatures']);
+                            foreach ($features as $variable) { ?>
+                                <li class="showcase-list">
+                                    <div class="sidebyside">
+                                        <ion-icon name="chevron-forward"></ion-icon>
+                                        <p> <?php echo $variable ?></p>
+                                    </div>
+                                </li>
+                            <?php } ?>
                         </ul>
                     </div>
                     <div class="sidebyside" style="justify-content: center; margin-top: 16px; ">
