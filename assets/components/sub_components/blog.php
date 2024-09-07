65315907cd79eaaec586b91ab114581b9eccae1c
Paramita <guptaparamita99@gmail.com>
2024-09-07 21:35:45 +0530

    edited About Us section

--- assets/components/sub_components/blog.php
+++ assets/components/sub_components/blog.php
@@ -3,57 +3,57 @@
         <div class="blog-container has-scrollbar">
           <div class="blog-card">
             <a href="#">
-              <img loading="lazy" src="./assets/images/blog-1.jpg"
+              <img loading="lazy" src="./assets/images/services/Yoga Class.jpg"
                 width="300" class="blog-banner">
             </a>
             <div class="blog-content">
-              <a href="#" class="blog-category">Lorem</a>
+              <a href="#" class="blog-category">Yoga Classes</a>
               <a href="#">
                 <h3 class="blog-title">Lorem ipsum dolor sit amet consecrate</h3>
               </a>
-              <p class="blog-meta"> By <cite>Mr Admin</cite> / <time datetime="2022-04-06">Apr 06, 2022</time>
+              <p class="blog-meta"> By <cite>Mr Sam  Goel</cite> / <time datetime="2022-04-06">Apr 06, 2022</time>
               </p>
             </div>
           </div>
           <div class="blog-card">
             <a href="#">
-              <img loading="lazy" src="./assets/images/blog-2.jpg"
+              <img loading="lazy" src="./assets/images/services/Dog Walking.jpg"
                 class="blog-banner" width="300">
             </a>
             <div class="blog-content">
-              <a href="#" class="blog-category">lorem</a>
+              <a href="#" class="blog-category">Pet Care</a>
               <h3>
                 <a href="#" class="blog-title">Lorem ipsum dolor sit amet consecrate</a>
               </h3>
-              <p class="blog-meta"> By <cite>Mr Robin</cite> / <time datetime="2022-01-18">Jan 18, 2022</time>
+              <p class="blog-meta"> By <cite>Mrs Vinita  Khaitan</cite> / <time datetime="2022-01-18">Jan 18, 2022</time>
               </p>
             </div>
           </div>
           <div class="blog-card">
             <a href="#">
-              <img loading="lazy" src="./assets/images/blog-3.jpg"
+              <img loading="lazy" src="./assets/images/services/Fashion Consulter.jpg"
                 class="blog-banner" width="300">
             </a>
             <div class="blog-content">
-              <a href="#" class="blog-category">lorem</a>
+              <a href="#" class="blog-category">Fashion Consultancy</a>
               <h3>
                 <a href="#" class="blog-title">Lorem ipsum dolor sit amet consecrate</a>
               </h3>
-              <p class="blog-meta"> By <cite>Mr Selsa</cite> / <time datetime="2022-02-10">Feb 10, 2022</time>
+              <p class="blog-meta"> By <cite>Mrs Shibani Dandekar</cite> / <time datetime="2022-02-10">Feb 10, 2022</time>
               </p>
             </div>
           </div>
           <div class="blog-card">
             <a href="#">
-              <img loading="lazy" src="./assets/images/blog-4.jpg"
+              <img loading="lazy" src="./assets/images/services/Elderly Care.jpg"
                 class="blog-banner" width="300">
             </a>
             <div class="blog-content">
-              <a href="#" class="blog-category">Lorem</a>
+              <a href="#" class="blog-category">Elderly Care</a>
               <h3>
                 <a href="#" class="blog-title">Lorem ipsum dolor sit amet consecrate</a>
               </h3>
-              <p class="blog-meta"> By <cite>Mr Pawar</cite> / <time datetime="2022-03-15">Mar 15, 2022</time>
+              <p class="blog-meta"> By <cite>Miss Isha Pillai</cite> / <time datetime="2022-03-15">Mar 15, 2022</time>
               </p>
             </div>
           </div>
