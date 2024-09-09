2f89396d0285a31dc5109cbf66bb8433c4b3afbd
Pritam <pritammahata12@gmail.com>
2024-09-09 15:57:53 +0530

    changing addCategory directory

---
+++ admin/addCategory.php
@@ -0,0 +1,11 @@
+<?php require("./components/header.php") ?>
+<main>
+    <div class="container-fluid px-4">
+        <h2 class="mt-4">Add New Category</h2>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item">Dashboard</li>
+            <li class="breadcrumb-item active">Category</li>
+        </ol>
+    </div>
+</main>
+<?php require('./components/footer.php') ?>
