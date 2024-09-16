7aec054f07e26ae0d5f2e7ba9953647aefef2b47
Pritam <pritammahata12@gmail.com>
2024-09-16 15:35:47 +0530

    refactor style

--- profile.php
+++ profile.php
@@ -45,9 +45,9 @@ if ($emailID) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
+    rel="stylesheet">
     <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@40,600,0,0" />
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@40,600,0,0" />
 </head>
 
 <body>
