bd360efc6dbfbca9df2f07a4208131f981d321ea
Pritam <pritammahata12@gmail.com>
2024-09-18 23:30:38 +0530

    adding image upload functionality

--- admin/modify.php
+++ admin/modify.php
@@ -5,8 +5,91 @@ if ($sid) {
     $rs = mysqli_query($conn, $src);
     $rec = mysqli_fetch_assoc($rs);
 } else {
-    $rec = ['sname' => '', 'sduration' => '', 'sprice' => '', 'sdiscount' => '', 'sdes' => '', 'sfeatures' => ''];
+    $rec = ['sname' => '', 'scategory' => '', 'sduration' => '', 'sprice' => '', 'sdiscount' => '', 'sdes' => '', 'sfeatures' => ''];
 }
+
+// image upload
+
+$simage = "";
+function upload_image()
+{
+    // The file path that includes the directory and the original file name
+    $targetDir = "../assets/images/services/";
+    $dataDir = "assets/images/services/" . basename($_FILES["simage"]["name"]);
+    $targetFile = $targetDir . basename($_FILES["simage"]["name"]);
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    $uploadOk = 1;
+    $check = getimagesize($_FILES["simage"]["tmp_name"]);
+
+    if ($check !== false) {
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+    // Check file size (limit to 5MB)
+    if ($_FILES["simage"]["size"] > 5000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    $allowedFormats = ['jpg', 'jpeg', 'png', 'gif'];
+    if (!in_array($imageFileType, $allowedFormats)) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    //     // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        // Try to upload file
+        if (move_uploaded_file($_FILES["simage"]["tmp_name"], $targetFile)) {
+            global $simage;
+            $simage = $dataDir;
+        }
+    }
+}
+
+if (isset($_POST['add'])) {
+    $sname = $_POST['sname'];
+    $scategory = $_POST['scategory'];
+    $sduration = $_POST['sduration'];
+    $sprice = $_POST['sprice'];
+    $sdiscount = $_POST['sdiscount'];
+    $sdes = $_POST['sdes'];
+    $sfeatures = $_POST['sfeatures'];
+    upload_image();
+    $sql = "INSERT INTO services (sname, sprice, sduration, sdes, sfeatures, scategory, simage, sdiscount,created_by) 
+            VALUES ('$sname', '$sprice', '$sduration', '$sdes', '$sfeatures', '$scategory', '$simage', '$sdiscount','1')";
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('Service Added Successfully')</script>";
+    } else {
+        echo "<script>alert('Error Adding Service')</script>";
+    }
+}
+if (isset($_POST['update'])) {
+    $sname = $_POST['sname'];
+    $scategory = $_POST['scategory'];
+    $sduration = $_POST['sduration'];
+    $sprice = $_POST['sprice'];
+    $sdiscount = $_POST['sdiscount'];
+    $sdes = $_POST['sdes'];
+    $sfeatures = $_POST['sfeatures'];
+    upload_image();
+    $sql = "UPDATE services SET sname='$sname', scategory = '$scategory', sduration='$sduration', sprice='$sprice', sdiscount='$sdiscount', sdes='$sdes', sfeatures='$sfeatures', simage = '$simage' WHERE sid=$sid";
+    if (mysqli_query($conn, $sql)) {
+        echo "<script>alert('Service Updated Successfully')</script>";
+    } else {
+        echo "<script>alert('Error Updating Service')</script>";
+    }
+}
+
+
+
 ?>
 <main>
     <div class="container-fluid px-4">
@@ -16,40 +99,52 @@ if ($sid) {
             <li class="breadcrumb-item active">Category</li>
         </ol>
         <div class="order-md-1 container">
-            <form class="needs-validation" novalidate="">
+            <form class="needs-validation" method="post" enctype="multipart/form-data">
                 <div class="row">
                     <div class="col-md-6 mb-3">
                         <label for="firstName">Service Name </label>
-                        <input type="text" class="form-control" id="firstName" placeholder="" value="<?php echo $rec['sname'] ?>" required="">
+                        <input type="text" class="form-control" id="firstName" placeholder="" name="sname" value="<?php echo $rec['sname'] ?>" required="">
                         <div class="invalid-feedback">
                             Valid first name is required.
                         </div>
                     </div>
-                    <!-- <div class="col-auto my-1">
+                    <div class="col-auto my-1">
                         <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
-                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                            <option selected>Choose...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                        <select class="form-select" id="scategory" name="scategory">
+                            <?php
+                            $sql = "SELECT subcat, subcatname FROM sub_category";
+                            $result = $conn->query($sql);
+                            if ($sid) {
+                                echo "<option selected ></option>";
+                            } else {
+                                echo "<option selected disabled>Select an option</option>";
+                            }
+                            if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {
+                                    echo "<option value='" . $row['subcat'] . "'>" . $row['subcatname'] . "</option>";
+                                }
+                            } else {
+                                echo "<option>No sub-categories found</option>";
+                            }
+                            ?>
                         </select>
-                    </div> -->
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-md-4 mb-3">
                         <label for="lastName">Duration</label>
-                        <input type="text" class="form-control" id="lastName" placeholder="hrs" value="<?php echo $rec['sduration'] ?>" required="">
+                        <input type="text" class="form-control" id="lastName" placeholder="hrs" name="sduration" value="<?php echo $rec['sduration'] ?>" required="">
                         <div class="invalid-feedback">
                             Valid last name is required.
                         </div>
                     </div>
                     <div class="col-md-4 mb-3">
                         <label for="address">Price</label>
-                        <input type="number" class="form-control" id="address" placeholder="price" required="" value="<?php echo $rec['sprice'] ?>">
+                        <input type="number" class="form-control" id="address" placeholder="price" required="" name="sprice" value="<?php echo $rec['sprice'] ?>">
                     </div>
                     <div class="col-md-4 mb-3">
                         <label for="address2">Discount<span class="text-muted">(Optional)</span></label>
-                        <input type="number" class="form-control" id="address2" placeholder="" value="<?php echo $rec['sdiscount'] ?>">
+                        <input type="number" class="form-control" id="address2" placeholder="" name="sdiscount" value="<?php echo $rec['sdiscount'] ?>">
                     </div>
                 </div>
                 <div class="mb-3">
@@ -57,15 +152,20 @@ if ($sid) {
                     <div class="input-group">
                         <div class="input-group-prepend">
                         </div>
-                        <input type="text" class="form-control" id="description" placeholder="Write a short description" required="" value="<?php echo $rec['sdes'] ?>">
+                        <input type="text" class="form-control" id="description" placeholder="Write a short description" required="" name="sdes" value="<?php echo $rec['sdes'] ?>">
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="features">Featurs</label>
-                    <input type="text" class="form-control" id="features" placeholder="write featurs separeted with ," value="
-                    <?php $text = trim($rec['sfeatures'], " ");
-                    echo $text; ?>
-                    ">
+                    <input type="text" class="form-control" id="features" placeholder="write featurs separeted with ," name="sfeatures" value="<?php echo $rec['sfeatures']; ?>">
+                    <!-- $rec['sfeatures'] -->
+                </div>
+                <div class="mb-3">
+                    <div class="custom-file">
+                        <label class="custom-file-label" for="customFile">Choose file</label>
+                        <!-- <input type="file" class="custom-file-input form-control" id="customFile"> -->
+                        <input type="file" class="form-control" id="simage" name="simage" accept="image/*" required>
+                    </div>
                 </div>
                 <?php
                 if ($sid) { ?>
