fbdb77858e322688252337e8d4fbe8a94b4568e9
Pritam <pritammahata12@gmail.com>
2024-09-24 22:41:08 +0530

    Refactor admin/components/header.php to remove unnecessary dropdown menu items

--- admin/components/header.php
+++ admin/components/header.php
@@ -42,11 +42,6 @@ if (empty($_SESSION['a_info'])) {
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i><?php echo $_SESSION['a_info']['aname'] ?></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
                     <li><a class="dropdown-item" href="components/logout.php">Logout</a></li>
                 </ul>
             </li>
