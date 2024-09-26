ff7f5ca935cd7448dc4a2a7a3e98885c4f35a0f9
PritamMahata <pritammahata12@gmail.com>
2024-09-26 23:40:36 +0530

    adding PHPMailer

---
+++ PHPMailer/verify.php
@@ -0,0 +1,22 @@
+<?php
+require_once('../env/config.php');
+if (isset($_GET['email']) && isset($_GET['v_code'])) {
+    $sql = "SELECT * FROM users WHERE email = '$_GET[email]' AND v_code = '$_GET[v_code]'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        if (mysqli_num_rows($result) == 1) {
+            $result_fetch = mysqli_fetch_assoc($result);
+            if ($result_fetch['v_code'] == $_GET['v_code']) {
+                $sql = "UPDATE users SET isverified = 1 WHERE email = '$_GET[email]'";
+                $result = mysqli_query($conn, $sql);
+                if ($result) {
+                    header("Location: ../login.php");
+                } else {
+                    header("Location: ../signup.php");
+                }
+            } else {
+                header("Location: ../signup.php");
+            }
+        }
+    }
+}
