ea785f00f7272fc527811946aa1fdbcf8687e28b
Pritam <pritammahata12@gmail.com>
2024-09-27 18:46:35 +0530

    Refactor email credentials in mail_config.php

--- PHPMailer/mail_config.php
+++ PHPMailer/mail_config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    // please Do not missuse this email address
+    // "Kindly use these credentials responsibly."
     'username' => 'servicehub36@gmail.com', // Replace with Your Gmail address
     'password' => 'hvle olnf kffi huae', // Replace with Your Gmail app password
 ];
