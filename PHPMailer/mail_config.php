cfa1e39536a83ab5174fe60b1a80f548da8603c5
Pritam <pritammahata12@gmail.com>
2025-01-08 22:38:28 +0530

    implemented dynamic server ip

--- PHPMailer/mail_config.php
+++ PHPMailer/mail_config.php
@@ -1,8 +1,13 @@
 <?php
+$output = shell_exec("ipconfig");
+preg_match_all('/IPv4 Address.*: (\d+\.\d+\.\d+\.\d+)/', $output, $matches);
+
+$ipAddresses = $matches[1];
+$server_ip = $ipAddresses[count($ipAddresses)-1];
 return [
     // "Kindly use these credentials responsibly."
     'username' => 'servicehub36@gmail.com', // Replace with Your Gmail address
     'password' => 'hvle olnf kffi huae', // Replace with Your Gmail app password
-    'server_ip' => 'http://127.0.0.1', // Replace with server ip password
+    'server_ip' => $server_ip, // Replace with server ip password
 ];
 ?>
