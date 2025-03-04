b8d19b5cec4383a99877847b782deef9f96c5385
PritamMahata <pritammahata12@gmail.com>
2025-03-04 20:58:57 +0530

    Update mail_config.php

--- PHPMailer/mail_config.php
+++ PHPMailer/mail_config.php
@@ -5,9 +5,8 @@ preg_match_all('/IPv4 Address.*: (\d+\.\d+\.\d+\.\d+)/', $output, $matches);
 $ipAddresses = $matches[1];
 $server_ip = $ipAddresses[count($ipAddresses)-1];
 return [
-    // "Kindly use these credentials responsibly."
-    'username' => 'servicehub36@gmail.com', // Replace with Your Gmail address
-    'password' => 'hvle olnf kffi huae', // Replace with Your Gmail app password
+    'username' => 'yourgamilId@gmail.com', // Replace with Your Gmail address
+    'password' => 'XXXX XXXX XXXX XXXX', // Replace with Your Gmail app password
     'server_url' => $server_ip, // Replace with server ip password
 ];
 ?>
